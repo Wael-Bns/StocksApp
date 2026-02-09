1af59d8902b9f1c5733d85a0d609b18fdd755008
Wael-Bns <w.waelbenslimene004@gmail.com>
2026-02-09 08:45:02 +0100

    remove all bin and obj folders from repository

--- StocksAppSolution/StocksApp.Core/DTO/StockTrade.cs
+++ StocksAppSolution/StocksApp.Core/DTO/StockTrade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,10 @@ namespace StocksApp.Core.DTO
 {
     public class StockTrade
     {
-
+        public string? StockSymbol { get; set; }
+        public string? StockName { get; set; }
+        public double Price { get; set; }
+        public uint Quantity { get; set; }
+        public string? Icon { get; set; }
     }
 }
--- StocksAppSolution/StocksApp.WebApi/Controllers/TradeController.cs
+++ StocksAppSolution/StocksApp.WebApi/Controllers/TradeController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using StocksApp.Core.DTO;
 using StocksApp.Core.ServiceContracts;
 
 namespace StocksApp.WebApi.Controllers
@@ -7,25 +9,10 @@ namespace StocksApp.WebApi.Controllers
     public class TradeController : CommonControllerBase
     {
         private readonly IFinnHubService _finnHubService;
-        
+
         public TradeController(IFinnHubService finnHubService)
         {
             _finnHubService = finnHubService;
-        }
-        
-        [HttpGet]
-        public async Task<IActionResult> GetCompanyProfile([FromQuery] string stockSymbol)
-        {
-            try
-            {
-                Dictionary<string, object> companyProfile = await _finnHubService.GetCompanyProfile(stockSymbol);
-                var jsonResult = JsonConvert.SerializeObject(companyProfile);
-                return Ok(jsonResult);
-            }
-            catch (Exception ex)
-            {
-                return Problem(ex.Message);
-            }
         }
     }
 }
