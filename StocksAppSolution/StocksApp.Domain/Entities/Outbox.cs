bcf8191a3176a8cf193938592c6e0d324a11ab3b
Wael-Bns <w.waelbenslimene004@gmail.com>
2026-07-16 12:43:46 +0100

    Add the outbox table

---
+++ StocksAppSolution/StocksApp.Domain/Entities/Outbox.cs
@@ -0,0 +1,11 @@
+﻿namespace StocksApp.Domain.Entities
+{
+    public class Outbox
+    {
+        public Guid OutboxId { get; set; }
+        public Type EventType { get; set; } = null!;
+        public string Payload { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; }
+        public DateTime? ProcessedAt { get; set; }
+    }
+}
