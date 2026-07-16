411747cb5c4dbc82507760ae413d3d0209e58e9e
Wael-Bns <w.waelbenslimene004@gmail.com>
2026-07-16 13:00:29 +0100

    Update the StockService.CreateSellOrder method to add the event to the outbox table .

--- StocksAppSolution/StocksApp.Core/Services/StockService.cs
+++ StocksAppSolution/StocksApp.Core/Services/StockService.cs
@@ -8,6 +8,7 @@ using StocksApp.Core.Exceptions;
 using StocksApp.Core.Helpers;
 using StocksApp.Core.HttpClientAbstractions;
 using StocksApp.Core.ServiceContracts;
+using StocksApp.Domain.Events;
 
 namespace StocksApp.Core.Services
 {
@@ -15,16 +16,22 @@ namespace StocksApp.Core.Services
     {
         private readonly IGenericRepository<BuyOrder> _buyOrderRepository;
         private readonly IGenericRepository<SellOrder> _sellOrderRepository;
+        private readonly IGenericRepository<Outbox> _outboxRepository;
+        private readonly IUnitOfWork _unitOfWork;
         private readonly IFinnHubHttpClient _finnhubHttpClient;
 
         public StockService(
             IGenericRepository<BuyOrder> buyOrderRepository,
             IGenericRepository<SellOrder> sellOrderRepository,
+            IGenericRepository<Outbox> outboxRepository,
+            IUnitOfWork unitOfWork,
             IFinnHubHttpClient finnHubHttpClient)
         {
             _buyOrderRepository = buyOrderRepository;
             _sellOrderRepository = sellOrderRepository;
             _finnhubHttpClient = finnHubHttpClient;
+            _outboxRepository = outboxRepository;
+            _unitOfWork = unitOfWork;
         }
 
         public async Task<BuyOrderResponse> CreateBuyOrder(BuyOrderAddRequest? buyOrderRequest, Guid userId)
@@ -46,9 +53,27 @@ namespace StocksApp.Core.Services
 
             var sellOrder = sellOrderRequest.ToSellOrder();
             sellOrder.UserId = userId;
+            try
+            {
+                // Begin transaction
+                await _unitOfWork.BeginTransactionAsync(CancellationToken.None);
+                
+                var createdSellOrder = await _sellOrderRepository.AddAsync(sellOrder);
+                var sellOrderCreatedCommand = createdSellOrder.ToSellOrderCreatedCommand();
+                var outboxEvent = sellOrderCreatedCommand.ToOutbox();
+                await _outboxRepository.AddAsync(outboxEvent);
+                
+                // Commit transaction
+                await _unitOfWork.CommitTransactionAsync(CancellationToken.None);
 
-            var createdSellOrder = await _sellOrderRepository.AddAsync(sellOrder);
-            return createdSellOrder.ToSellOrderResponse();
+                return createdSellOrder.ToSellOrderResponse();
+            }
+            catch (Exception ex)
+            {
+                // Rollback transaction in case of an error
+                await _unitOfWork.RollbackTransactionAsync(CancellationToken.None);
+                throw new Exception("An error occurred while creating the sell order and saving the outbox event.", ex);
+            }
         }
 
         public async Task<List<BuyOrderResponse>> GetBuyOrdersByUser(Guid userId)
--- StocksAppSolution/StocksApp.Domain/Entities/Outbox.cs
+++ StocksAppSolution/StocksApp.Domain/Entities/Outbox.cs
@@ -8,4 +8,17 @@
         public DateTime CreatedAt { get; set; }
         public DateTime? ProcessedAt { get; set; }
     }
+    public static class OutboxExtensions
+    {
+        public static Outbox ToOutbox(this object @event)
+        {
+            return new Outbox
+            {
+                OutboxId = Guid.NewGuid(),
+                EventType = @event.GetType(),
+                Payload = System.Text.Json.JsonSerializer.Serialize(@event),
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+    }
 }
