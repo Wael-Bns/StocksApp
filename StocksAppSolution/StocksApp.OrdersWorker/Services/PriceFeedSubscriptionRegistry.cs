628d119d74e365ef8ca56fc31175df5c23423499
Wael-Bns <w.waelbenslimene004@gmail.com>
2026-09-21 19:01:14 +0100

    Add Polly retry policy after unsuccessful subscribe/unsubscribe

--- StocksAppSolution/StocksApp.OrdersWorker/Services/PriceFeedSubscriptionRegistry.cs
+++ StocksAppSolution/StocksApp.OrdersWorker/Services/PriceFeedSubscriptionRegistry.cs
@@ -1,4 +1,7 @@
-﻿using StocksApp.Core.WebSocketClientAbstractions;
+﻿using Polly;
+using Polly.Registry;
+using StocksApp.Core.WebSocketClientAbstractions;
+using StocksApp.OrdersWorker.Resilience;
 using StocksApp.OrdersWorker.ServiceContracts;
 
 namespace StocksApp.OrdersWorker.Services
@@ -7,10 +10,13 @@ namespace StocksApp.OrdersWorker.Services
     {
         private readonly IFinnhubWebSocketClient _finnhubWebSocketClient;
         private readonly HashSet<string> _subscribedStockSymbols;
-        public PriceFeedSubscriptionRegistry(IFinnhubWebSocketClient finnhubWebSocketClient)
+        private readonly ResiliencePipeline _pipeline;
+        public PriceFeedSubscriptionRegistry(IFinnhubWebSocketClient finnhubWebSocketClient,
+            ResiliencePipelineProvider<string> pipelineProvider)
         {
             _finnhubWebSocketClient = finnhubWebSocketClient;
             _subscribedStockSymbols = new HashSet<string>();
+            _pipeline = pipelineProvider.GetPipeline(ResilienceOptions.PriceFeedSubscriptionPipeline);
         }
 
         public async Task EnsureSubscribedAsync(string stockSymbol, CancellationToken cancellationToken)
@@ -18,14 +24,17 @@ namespace StocksApp.OrdersWorker.Services
             if (string.IsNullOrEmpty(stockSymbol) || _subscribedStockSymbols.Contains(stockSymbol))
                 return;
 
-            await _finnhubWebSocketClient.SubscribeAsync(stockSymbol, cancellationToken);
+            await _pipeline.ExecuteAsync(
+            async token => await _finnhubWebSocketClient.SubscribeAsync(stockSymbol, token), cancellationToken);
+
             _subscribedStockSymbols.Add(stockSymbol);
         }
         public async Task UnsubscribeAsync(string stockSymbol, CancellationToken cancellationToken)
         {
             if (!string.IsNullOrEmpty(stockSymbol) && _subscribedStockSymbols.Remove(stockSymbol))
             {
-                await _finnhubWebSocketClient.UnsubscribeAsync(stockSymbol, cancellationToken);
+                await _pipeline.ExecuteAsync(
+                    async token => await _finnhubWebSocketClient.UnsubscribeAsync(stockSymbol, token), cancellationToken);
             }
         }
     }
