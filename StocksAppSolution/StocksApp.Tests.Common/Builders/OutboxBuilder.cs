a4c6a8438793e607e25bd474a341601bc9689f74
Wael-Bns <w.waelbenslimene004@gmail.com>
2026-09-22 19:14:25 +0100

    Add the 'OutboxProcessor' unit tests

---
+++ StocksAppSolution/StocksApp.Tests.Common/Builders/OutboxBuilder.cs
@@ -0,0 +1,57 @@
+﻿using StocksApp.Domain.Entities;
+using StocksApp.Domain.Enums;
+
+namespace StocksApp.Tests.Common.Builders
+{
+    public class OutboxBuilder
+    {
+        private Guid _outboxId = Guid.NewGuid();
+        private string _eventName = "SellOrderCreated";
+        private string _payload = "{}";
+        private int _retryCount = 0;
+        private OutboxStatus _status = OutboxStatus.Pending;
+
+        public OutboxBuilder WithOutboxId(Guid outboxId)
+        {
+            _outboxId = outboxId;
+            return this;
+        }
+
+        public OutboxBuilder WithEventName(string eventName)
+        {
+            _eventName = eventName;
+            return this;
+        }
+
+        public OutboxBuilder WithPayload(string payload)
+        {
+            _payload = payload;
+            return this;
+        }
+
+        public OutboxBuilder WithRetryCount(int retryCount)
+        {
+            _retryCount = retryCount;
+            return this;
+        }
+
+        public OutboxBuilder WithStatus(OutboxStatus status)
+        {
+            _status = status;
+            return this;
+        }
+
+        public Outbox Build()
+        {
+            return new Outbox
+            {
+                OutboxId = _outboxId,
+                EventName = _eventName,
+                Payload = _payload,
+                RetryCount = _retryCount,
+                Status = _status,
+                CreatedAt = DateTime.UtcNow
+            };
+        }
+    }
+}
---
+++ StocksAppSolution/StocksApp.Tests.Common/Builders/OutboxNotificationBuilder.cs
@@ -0,0 +1,40 @@
+﻿using System.Text.Json;
+using StocksApp.Domain.Notifications;
+
+namespace StocksApp.Tests.Common.Builders
+{
+    public class OutboxNotificationBuilder
+    {
+        private Guid _outboxId = Guid.NewGuid();
+        private string _eventName = "SellOrderCreated";
+        private JsonElement _payload = JsonDocument.Parse("{}").RootElement;
+
+        public OutboxNotificationBuilder WithOutboxId(Guid outboxId)
+        {
+            _outboxId = outboxId;
+            return this;
+        }
+
+        public OutboxNotificationBuilder WithEventName(string eventName)
+        {
+            _eventName = eventName;
+            return this;
+        }
+
+        public OutboxNotificationBuilder WithPayload(string json)
+        {
+            _payload = JsonDocument.Parse(json).RootElement;
+            return this;
+        }
+
+        public OutboxNotification Build()
+        {
+            return new OutboxNotification
+            {
+                OutboxId = _outboxId,
+                EventName = _eventName,
+                Payload = _payload
+            };
+        }
+    }
+}
